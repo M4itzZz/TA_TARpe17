f4e9dd127129e4032e742d45f460036c9d6c87b2
Maido Kaiste <kaistemaido@gmail.com>
2019-05-09 11:43:26 +0300

    Esimese ülesande lahendamine

---
+++ Kangelased/Kangelased/Kangelased.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kangelased
+{
+    class Kangelased
+    {
+        private string _Nimi;
+        private string _Asukoht;
+
+        public Kangelased(string Nimi, string Asukoht)
+        {
+            _Nimi = Nimi;
+            _Asukoht = Asukoht;
+        }
+
+        public string Nimi { get => Nimi; set => Nimi = value; }
+        public string Asukoht { get => Asukoht; set => Asukoht = value; }   
+
+        public int Päästa(int Ohustatuid)
+        {
+            return (int)Math.Round(Ohustatuid * .95);
+        }
+
+        public override string ToString()
+        {
+            return Nimi + " hoiab " + Asukoht + "turvalisem";
+        }
+    }
+}

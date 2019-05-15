4c46ef0a255b341a1b40fbdf26b6b25cfff9068d
Maido Kaiste <kaistemaido@gmail.com>
2019-05-15 14:55:54 +0300

    Loome Kangelase klassi meetodid

--- Kangelased/Kangelased/Kangelane.cs
+++ Kangelased/Kangelased/Kangelane.cs
@@ -11,7 +11,7 @@ namespace Kangelased
         private string _Nimi;
         private string _superNimi;
         private string _v6ime;
-        public Kangelane(string Nimi,string superNimi,string v6ime)
+        public Kangelane(string Nimi, string superNimi, string v6ime)
         {
             _Nimi = Nimi;
             _superNimi = superNimi;
@@ -21,5 +21,21 @@ namespace Kangelased
         public string Nimi { get => _Nimi; set => _Nimi = value; }
         public string SuperNimi { get => _superNimi; set => _superNimi = value; }
         public string V6ime { get => _v6ime; set => _v6ime = value; }
+
+        public string AnnaNimi()
+        {
+            return Nimi;
+        }
+
+        public string AnnaSuperNimi()
+        {
+            return SuperNimi;
+        }
+
+        public string AnnaV6ime()
+        {
+            return V6ime;
+        }
     }
 }
+
